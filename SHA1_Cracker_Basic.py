d9df71c2278f59b7dee568731a9a505f1fefe2e1
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-02-25 15:43:36 -0500

    updated logging for multiword hash.

--- SHA1_Cracker_Basic.py
+++ SHA1_Cracker_Basic.py
@@ -1,60 +1,82 @@
 import hashlib
 import datetime
+import sys
 
-# part a hash: b7a875fc1ea228b9061041b7cec4bd3c52ab3ce3
-# part b hash: 801cdea58224c921c21fd2b183ff28ffa910ce31
-# part c hash: ece4bb07f2580ed8b39aa52b7f7f918e43033ea1
-# part c salt: f0744d60dd500c92c0d37c16174cc58d3c4bdd8e
-# part d hash: 34302959e138917ce9339c0b30ec50e650ce6b40
 with open('./10-million-password-list-top-1000000.txt') as f:
     lines = f.read().splitlines()
 
-goalHash = "ece4bb07f2580ed8b39aa52b7f7f918e43033ea1".lower()
-hashOfSalt = "f0744d60dd500c92c0d37c16174cc58d3c4bdd8e"
+goalHash = ""
+hashOfSalt = ""
 unhashedSalt = ""
-salted = True
+salted = False
 multiWord = False
 attemptCount = 0
 attemptCountSalt = 0
-startTime = datetime.datetime.now()
+solved = False
 
-if not salted and not multiWord:
-    print("Unsalted single word")
-    for x in lines:
-        attemptCount += 1
-        if hashlib.sha1(x).hexdigest() == goalHash:
-            print("Password is: " + x)
-            break
-elif not multiWord and salted:
-    print("Salted single word")
-    for x in lines:
-        attemptCountSalt += 1
-        if hashlib.sha1(x).hexdigest() == hashOfSalt:
-            unhashedSalt = x
-            print x
-            break
-    if unhashedSalt != "":
-        for x in lines:
-            attemptCount += 1
-            if hashlib.sha1(unhashedSalt + x).hexdigest() == goalHash:
-                print("Password is: " + x)
-                break
+if len(sys.argv) > 1:
+    goalHash = sys.argv[1].lower()
+    saltedQuestion = raw_input("Is this hash salted? (y/n) ")
+    if saltedQuestion == 'y':
+        salted = True
+        saltQuestion = raw_input("What is your salt? ")
+        hashOfSalt = saltQuestion.lower()
+    multiWordQuestion = raw_input("Does this hash consist of two dictionary words divided by a space? (y/n) ")
+    if multiWordQuestion == 'y':
+        multiWord = True
+    if salted and multiWord:
+        print "This hash can not be brute forced by this program because it is a salted combination of dictionary terms."
     else:
-        print("Could not solve salt")
-elif multiWord and not salted:
-    print(str(datetime.datetime.now() - startTime))
-    for x in lines:
-        for y in lines:
-            if hashlib.sha1(x + " " + y).hexdigest() == goalHash:
-                print("found")
-                print(x + " " + y)
+        startTime = datetime.datetime.now()
+        if not salted and not multiWord:
+            print("Solving unnsalted single word")
+            for x in lines:
+                attemptCount += 1
+                if hashlib.sha1(x).hexdigest() == goalHash:
+                    print("Password is: " + x)
+                    solved = True
+                    break
+        elif not multiWord and salted:
+            print("Solving salted single word")
+            for x in lines:
+                attemptCountSalt += 1
+                if hashlib.sha1(x).hexdigest() == hashOfSalt:
+                    unhashedSalt = x
+                    print "Salt is: " + x
+                    print "Took " + str(attemptCountSalt) + " attempts to crack salt."
+                    break
+            if unhashedSalt != "":
+                for x in lines:
+                    attemptCount += 1
+                    if hashlib.sha1(unhashedSalt + x).hexdigest() == goalHash:
+                        print("Password is: " + x)
+                        solved = True
+                        break
+            else:
+                print("Could not solve salt")
+        elif multiWord and not salted:
+            for x in lines:
+                for y in lines:
+                    attemptCount += 1
+                    if hashlib.sha1(x + " " + y).hexdigest() == goalHash:
+                        print("Password is " + x + " " + y)
+                        solved = True
+                        break
+                else:
+                    hashPerSecond = (float(attemptCount) / (datetime.datetime.now() - startTime).total_seconds())
+                    myString = ("Time: " + str(datetime.datetime.now() - startTime) + " | Attempts: " + str(
+                        attemptCount) +
+                                " | Hashrate: " + str(hashPerSecond) + " hash/s | Possible passwords remaining: " +
+                                str((len(lines) ** 2) - attemptCount) + " | Estimated time remaining: " +
+                                str(((len(lines) ** 2) - attemptCount) / hashPerSecond) + " seconds")
+                    sys.stdout.write('\r'+str(myString))
+                    sys.stdout.flush()
+                    continue
                 break
+        if solved:
+            print("Time taken: " + str(datetime.datetime.now() - startTime))
+            print("Took " + str(attemptCount) + " attempts to crack input hash.")
         else:
-            print(str(datetime.datetime.now() - startTime))
-            continue
-        break
+            print "Hash not resolvable with current dictionary and parameters."
 else:
-    print("Cannot handle input.")
-
-print("Time taken: " + str(datetime.datetime.now() - startTime))
-print("Took " + str(attemptCount) + " attempts.")
+    print "Please input the desired hash value to crack as a argument when you run the  application."
